f191405c96076d67d5d44ad65070e7d893345397
Jan Weiß <jan@geheimwerk.de>
2012-09-20 11:53:46 +0200

    Improving update.sh.

--- discount-config/update.sh
+++ discount-config/update.sh
@@ -22,15 +22,17 @@ cd `dirname $0`/../External/discount/
 
 status_msg "Copying important files..."
 
+DISCOUNT_CONFIG_DIR="../../discount-config"
+
 if head -n 1 config.h | grep -q "^/\*$"; then
 	# remove generated comments in config.h
-	sed '1,/^ *\*\/ *$/ { d; }' < config.h >../../discount-config/config.h && echo 'config.h'
+	sed '1,/^ *\*\/ *$/ { d; }' < config.h >"$DISCOUNT_CONFIG_DIR/config.h" && echo 'config.h'
 else
-	cp config.h ../../discount-config/config.h && echo 'config.h'
+	cp config.h "$DISCOUNT_CONFIG_DIR/config.h" && echo 'config.h'
 	error_msg "Can't locate config.h comments!"
 	error_msg "Check the diff before committing (and fix this script if you can)"
 fi
-cp mkdio.h ../../discount-config/mkdio.h && echo 'mkdio.h'
+cp mkdio.h "$DISCOUNT_CONFIG_DIR/mkdio.h" && echo 'mkdio.h'
 rm ../../blocktags # Will be recreated by mktags (Run blocktags Script build phase)
 
 status_msg "Clean files from working copy..."
